96fa238b91e79f9f515d87a4b74628909ce45291
ol3gatron <olegatron@gmail.com>
2022-10-29 16:37:42 +0300

    seted up routes for home, movie details and page not found components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import "./App.scss"
+import { Routes, Route } from "react-router-dom"
+
+import Header from "./components/Header/Header"
+import Home from "./components/Home/Home"
+import MovieDetails from "./components/MovieDetails/MovieDetails"
+import PageNotFound from "./components/PageNotFound/PageNotFound"
+import Footer from "./components/Footer/Footer"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="app">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:imdbID" element={<MovieDetails />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </div>
   )
 }
 export default App
