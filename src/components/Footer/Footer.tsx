e2ffe10d773792be89293d436bd0f8bf8797a10f
ol3gatron <olegatron@gmail.com>
2022-10-29 17:33:50 +0300

    created styles for footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,11 @@
+import "./Footer.scss"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div>Movie App</div>
+      <div>©2022, Made with ❤️ by <a href="https://github.com/ol3gatron" target="_blank">Oleg</a></div>
+    </footer>
   )
 }
 export default Footer
