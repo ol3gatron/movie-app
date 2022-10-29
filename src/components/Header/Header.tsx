6dc340d1f4a4a1d13c755295abc57985bbf43938
ol3gatron <olegatron@gmail.com>
2022-10-29 17:22:23 +0300

    created styles for header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,18 @@
+import { Link } from "react-router-dom"
+import "./Header.scss"
+
+import user from "../../images/user.png"
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <Link to="/">
+        <div className="logo">Movie App</div>
+      </Link>
+      <div className="user-image">
+        <img src={user} alt="User image" />
+      </div>
+    </header>
   )
 }
 export default Header
