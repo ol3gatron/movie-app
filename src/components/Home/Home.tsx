e6e419bceb72a057dc50efb978c1b4c20b9ef050
ol3gatron <olegatron@gmail.com>
2022-10-29 17:55:10 +0300

    created getMoviesData function using useEffect hook in home component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,28 @@
+import { useEffect } from "react"
+
+import MovieListing from "../MovieListing/MovieListing"
+import movieApi from "../../common/apis/movieApi"
+import { API_KEY } from "../../common/apis/movieApiKey"
+
 const Home = () => {
+  useEffect(() => {
+    const movieTest = "Harry"
+    try {
+      const getMoviesData = async () => {
+        const res = await movieApi.get(`?apiKey=${API_KEY}&s=${movieTest}&type=movie`)
+        console.log("Response:", res)
+      }
+      getMoviesData()
+    } catch (error) {
+      console.log("Error: ", error)
+    }
+  }, [])
+
   return (
-    <div>Home</div>
+    <main>
+      <div className="banner-img"></div>
+      <MovieListing />
+    </main>
   )
 }
 export default Home
