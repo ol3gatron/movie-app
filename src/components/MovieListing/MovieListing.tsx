8312f10bfb8adce53767a73ce7ace1cafaf7f960
ol3gatron <olegatron@gmail.com>
2022-10-31 15:15:32 +0300

    provided data to MovieListing component via useSelector hook

--- src/components/MovieListing/MovieListing.tsx
+++ src/components/MovieListing/MovieListing.tsx
@@ -1,6 +1,39 @@
+import { useSelector } from "react-redux"
+import { getAllMovies } from "../../features/movies/movieSlice"
+import MovieCard from "../MovieCard/MovieCard"
+
+interface Movie {
+  Title: string,
+  Year: string,
+  imdbID: string,
+  type: string,
+  Poster: string,
+}
+
 const MovieListing = () => {
+  const movies = useSelector(getAllMovies)
+
+  const renderMovies = movies.Response === "True" ? (
+    <>
+      {movies.Search.map((movie: Movie, index: number) => (
+        <MovieCard key={index} data={movie} />
+      ))}
+    </>
+  ) : (
+    <div className="movies-error">
+      <h3>{movies.Error}</h3>
+    </div>
+  )
+
   return (
-    <div>MovieListing</div>
+    <div className="movie-wrapper">
+      <div className="movie-list">
+        <h2>Movies</h2>
+        <div className="movie-container">
+          {renderMovies}
+        </div>
+      </div>
+    </div>
   )
 }
 export default MovieListing
