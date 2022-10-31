105a14ae621ad678a5e56726d853670e548b89ac
ol3gatron <olegatron@gmail.com>
2022-10-31 15:51:51 +0300

    created grid container for MovieListing component

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,6 +1,32 @@
-const MovieCard = () => {
+interface Movie {
+  Title: string,
+  Year: string,
+  imdbID: string,
+  type: string,
+  Poster: string,
+}
+
+interface Props {
+  data: Movie
+}
+
+const MovieCard = ({data}: Props) => {
+  console.log(data)
+
   return (
-    <div>MovieCard</div>
+    <div className="card-item">
+      <div className="card-inner">
+        <div className="card-top">
+          <img src={data.Poster} alt={data.Title} />
+        </div>
+        <div className="card-bottom">
+          <div className="card-info">
+            <h4>{data.Title}</h4>
+            <p>{data.Year}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 export default MovieCard
