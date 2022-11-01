d9a34e705f48f057df092a91f1a8e2c12b0f10e1
ol3gatron <olegatron@gmail.com>
2022-11-01 14:32:58 +0300

    created renderShows element in MovieListing component that rendres shows

--- src/components/MovieListing/MovieListing.tsx
+++ src/components/MovieListing/MovieListing.tsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux"
 
-import { getAllMovies } from "../../features/movies/movieSlice"
+import { getAllMovies, getAllShows } from "../../features/movies/movieSlice"
 import MovieCard from "../MovieCard/MovieCard"
 import "./MovieListing.scss"
 
@@ -14,10 +14,23 @@ interface Movie {
 
 const MovieListing = () => {
   const movies = useSelector(getAllMovies)
+  const shows = useSelector(getAllShows)
 
   const renderMovies = movies.Response === "True" ? (
     <>
       {movies.Search.map((movie: Movie, index: number) => (
+        <MovieCard key={index} data={movie} />
+      ))}
+    </>
+  ) : (
+    <div className="movies-error">
+      <h3>{movies.Error}</h3>
+    </div>
+  )
+
+  const renderShows = shows.Response === "True" ? (
+    <>
+      {shows.Search.map((movie: Movie, index: number) => (
         <MovieCard key={index} data={movie} />
       ))}
     </>
@@ -33,6 +46,12 @@ const MovieListing = () => {
         <h2>Movies</h2>
         <div className="movie-container">
           {renderMovies}
+        </div>
+      </div>
+      <div className="show-list">
+        <h2>Shows</h2>
+        <div className="movie-container">
+          {renderShows}
         </div>
       </div>
     </div>
