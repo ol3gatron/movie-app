c541eb6aaea6379bf351e3e8519255f6a5751890
ol3gatron <olegatron@gmail.com>
2022-11-02 17:49:29 +0300

    fixed styles in Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer>
       <div>Movie App</div>
-      <div>©2022, Made with ❤️ by <a href="https://github.com/ol3gatron" target="_blank">Oleg</a></div>
+      <div className="credits">©2022, Made with ❤️ by <a href="https://github.com/ol3gatron" target="_blank">Oleg</a></div>
     </footer>
   )
 }
